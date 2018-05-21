e058b70b332bbf7c2c9e909bf6488d654b3cde5e
rsho6991 <rsho6991@gmail.com>
2018-05-21 22:02:10 +0300

    php

---
+++ economics.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="style/style.css">
+		<title>Економіка</title>
+	</head>
+	<body>
+		<header>
+			<div class="logo"><a href="index.php"><img src="img/logo.jpg" alt="Логотип" width="100px" height="100px"></a></div>
+			<div class="h1"><h1>AltEn</h1></div>
+			<div class="tel">
+				<h3>+380986431883</h3>
+				<h3>+380993337400</h3>
+			</div>
+		</header>
+		<div class="center">
+			<div class="mainimg">
+				<img src="img/main.jpg" alt="Зображення">
+			</div>
+			<main>
+				<div class="button">
+					<button type="button" onClick='location.href="info.php"' class="fb">Інформація</button>
+					<button type="button" onClick='location.href="offer.php"'>Пропозиція</button>
+					<button type="button" onClick='location.href="production.php"'>Продукція</button>
+					<button type="button" onClick='location.href="economics.php"'>Економіка</button>
+					<button type="button" onClick='location.href="gallery.php"' class="lb">Галерея</button>
+				</div>
+			</main>
+		</div>
+		<footer>
+			<p>AltEn</p>
+		</footer>
+	</body>
+</html>
